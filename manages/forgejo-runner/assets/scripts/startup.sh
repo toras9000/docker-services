71aaf5ca444946929540373ece0c00d6735596f5
toras9000 <toras9000@example.com>
2026-02-08 19:35:38 +0900

    docker manage files

---
+++ manages/forgejo-runner/assets/scripts/startup.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+template_config=/assets/configs/config.yml
+service_config=/assets/configs/service.yml
+runner_config=/data/config.yml
+
+set -eu
+
+echo "Prepare config"
+cp  "$template_config" "$runner_config"
+
+if [ ! -f "$service_config" ]; then
+    echo "Waiting for runner setup ..."
+    while [ ! -f "$service_config" ]
+    do
+        sleep 1s
+    done
+
+    echo "Waiting for setup complete ..."
+    sleep 5s
+fi
+
+echo "Apply service config"
+sed '1s/^\xef\xbb\xbf//' "$service_config" >> "$runner_config"
+
+echo "Start runner daemon"
+forgejo-runner daemon --config "$runner_config"
